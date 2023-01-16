9f0494a7d28c4b2f10db8d7da997ff1312adcd69
dteodore <35232980+dteodore@users.noreply.github.com>
2023-01-16 15:30:55 -0700

    Create preprocess_wiki_words.py

---
+++ src/preprocess_wiki_words.py
@@ -0,0 +1,28 @@
+from nltk.tokenize import sent_tokenize
+import re
+
+def splitWriteSents(filename, filename_output):
+    #not_new_article = ['= De <unk> a <unk> : interviews with persons affected by the 2010 Haiti earthquake =', '= 14.1 years ) , <unk> ( t1 / 2 =', '= Played , W =', '= Losses , D =', '= Ties , A =', '= <unk> points , <unk> =', '= <unk> / <unk> , Pts =', '= Played , W =', '= Ties , L =', '= No result , Pts =', '= Played , W =', '= Losses , T =', '= No result , Pts =', '= Played , W =', '= Ties , L =', '= No result , Pts =', '= Played , W =', '= Losses , T =', '= No result , Pts =', '= 10.6 ° compared to 7 ° for Mercury and 17 ° for Pluto ) and moderately eccentric ( e =', '= and =', '= 7 / 2 ) <unk> the <unk> , which is induced by eliminating this local moment ( J =']
+    org_lines = open(filename, 'r').readlines()
+    f = open(filename_output, 'w')
+    for line in org_lines:
+        line = line.strip()
+        line = re.sub(r" @", r"", line)
+        line = re.sub(r"@ ", r"", line)
+
+        matchObj = re.match(r'^=\s[^=;]+\s=$', line) # match title
+        if not matchObj:  
+            prts = sent_tokenize(line)
+            for prt in prts:
+                if prt != "" and prt != "\n":
+                    f.write(prt.strip() + "\n")
+
+        
+    f.close()
+    return
+
+if __name__ == "__main__":
+    filename = '/home/dteodore/projects/def-cepp/dteodore/wikitext2/test.txt'
+    filename_output = '/home/dteodore/projects/def-cepp/dteodore/wiki_test_clean.txt'
+
+    splitWriteSents(filename, filename_output)
